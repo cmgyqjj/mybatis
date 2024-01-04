ee43b4984e401b92704cc54b02084bb035ebd836
cmgyqjj <qjjypa@qq.com>
2024-01-04 13:35:19 +0800

    添加了一些注解，设计到缓存部分，这里主要是缓存的基础Cache接口相关，而并非整个一级二级缓存设计

--- mybatis-3-mybatis-3.5.13/src/main/java/org/apache/ibatis/cache/Cache.java
+++ mybatis-3-mybatis-3.5.13/src/main/java/org/apache/ibatis/cache/Cache.java
@@ -41,61 +41,27 @@ import java.util.concurrent.locks.ReadWriteLock;
 
 public interface Cache {
 
-  /**
-   * @return The identifier of this cache
-   */
-  String getId();
+//    获取缓存对象的Id
+    String getId();
 
-  /**
-   * @param key
-   *          Can be any object but usually it is a {@link CacheKey}
-   * @param value
-   *          The result of a select.
-   */
-  void putObject(Object key, Object value);
+//    放缓存对象
+    void putObject(Object key, Object value);
 
-  /**
-   * @param key
-   *          The key
-   *
-   * @return The object stored in the cache.
-   */
-  Object getObject(Object key);
+//    获取缓存对象
+    Object getObject(Object key);
 
-  /**
-   * As of 3.3.0 this method is only called during a rollback for any previous value that was missing in the cache. This
-   * lets any blocking cache to release the lock that may have previously put on the key. A blocking cache puts a lock
-   * when a value is null and releases it when the value is back again. This way other threads will wait for the value
-   * to be available instead of hitting the database.
-   *
-   * @param key
-   *          The key
-   *
-   * @return Not used
-   */
-  Object removeObject(Object key);
+//    删除缓存对象
+    Object removeObject(Object key);
 
-  /**
-   * Clears this cache instance.
-   */
-  void clear();
+//    清空缓存
+    void clear();
 
-  /**
-   * Optional. This method is not called by the core.
-   *
-   * @return The number of elements stored in the cache (not its capacity).
-   */
-  int getSize();
+//    获取缓存的大小
+    int getSize();
 
-  /**
-   * Optional. As of 3.2.6 this method is no longer called by the core.
-   * <p>
-   * Any locking needed by the cache must be provided internally by the cache provider.
-   *
-   * @return A ReadWriteLock
-   */
-  default ReadWriteLock getReadWriteLock() {
-    return null;
-  }
+//    获取读写锁，因为不会被Mybatis核心使用，所以提供空实现
+    default ReadWriteLock getReadWriteLock() {
+        return null;
+    }
 
 }
--- mybatis-3-mybatis-3.5.13/src/main/java/org/apache/ibatis/cache/decorators/BlockingCache.java
+++ mybatis-3-mybatis-3.5.13/src/main/java/org/apache/ibatis/cache/decorators/BlockingCache.java
@@ -36,92 +36,102 @@ import org.apache.ibatis.cache.CacheException;
  */
 public class BlockingCache implements Cache {
 
-  private long timeout;
-  private final Cache delegate;
-  private final ConcurrentHashMap<Object, CountDownLatch> locks;
+    //    阻塞超时时间
+    private long timeout;
+//    被装饰的底层Cache对象
+    private final Cache delegate;
+//    每个key有对应的锁
+    private final ConcurrentHashMap<Object, CountDownLatch> locks;
 
-  public BlockingCache(Cache delegate) {
-    this.delegate = delegate;
-    this.locks = new ConcurrentHashMap<>();
-  }
-
-  @Override
-  public String getId() {
-    return delegate.getId();
-  }
-
-  @Override
-  public int getSize() {
-    return delegate.getSize();
-  }
-
-  @Override
-  public void putObject(Object key, Object value) {
-    try {
-      delegate.putObject(key, value);
-    } finally {
-      releaseLock(key);
+    public BlockingCache(Cache delegate) {
+        this.delegate = delegate;
+        this.locks = new ConcurrentHashMap<>();
     }
-  }
 
-  @Override
-  public Object getObject(Object key) {
-    acquireLock(key);
-    Object value = delegate.getObject(key);
-    if (value != null) {
-      releaseLock(key);
+    @Override
+    public String getId() {
+        return delegate.getId();
     }
-    return value;
-  }
 
-  @Override
-  public Object removeObject(Object key) {
-    // despite its name, this method is called only to release locks
-    releaseLock(key);
-    return null;
-  }
+    @Override
+    public int getSize() {
+        return delegate.getSize();
+    }
 
-  @Override
-  public void clear() {
-    delegate.clear();
-  }
-
-  private void acquireLock(Object key) {
-    CountDownLatch newLatch = new CountDownLatch(1);
-    while (true) {
-      CountDownLatch latch = locks.putIfAbsent(key, newLatch);
-      if (latch == null) {
-        break;
-      }
-      try {
-        if (timeout > 0) {
-          boolean acquired = latch.await(timeout, TimeUnit.MILLISECONDS);
-          if (!acquired) {
-            throw new CacheException(
-                "Couldn't get a lock in " + timeout + " for the key " + key + " at the cache " + delegate.getId());
-          }
-        } else {
-          latch.await();
+    @Override
+    public void putObject(Object key, Object value) {
+        try {
+            delegate.putObject(key, value);
+        } finally {
+            releaseLock(key);
         }
-      } catch (InterruptedException e) {
-        throw new CacheException("Got interrupted while trying to acquire lock for key " + key, e);
-      }
     }
-  }
 
-  private void releaseLock(Object key) {
-    CountDownLatch latch = locks.remove(key);
-    if (latch == null) {
-      throw new IllegalStateException("Detected an attempt at releasing unacquired lock. This should never happen.");
+    @Override
+    public Object getObject(Object key) {
+//        尝试获取锁
+        acquireLock(key);
+//        取到缓存对象
+        Object value = delegate.getObject(key);
+//        不为空的话就释放锁，否则就继续持有
+        if (value != null) {
+            releaseLock(key);
+        }
+        return value;
     }
-    latch.countDown();
-  }
 
-  public long getTimeout() {
-    return timeout;
-  }
+    @Override
+    public Object removeObject(Object key) {
+        // despite its name, this method is called only to release locks
+        releaseLock(key);
+        return null;
+    }
 
-  public void setTimeout(long timeout) {
-    this.timeout = timeout;
-  }
+    @Override
+    public void clear() {
+        delegate.clear();
+    }
+
+    private void acquireLock(Object key) {
+//        写屏障
+        CountDownLatch newLatch = new CountDownLatch(1);
+        while (true) {
+//            尝试把锁放进去，如果没有对应的对象就创建一个，之前用的是ReentrantLock
+            CountDownLatch latch = locks.putIfAbsent(key, newLatch);
+            if (latch == null) {
+                break;
+            }
+            try {
+//                设置阻塞超时时间然后阻塞，或者不设置直接阻塞
+                if (timeout > 0) {
+                    boolean acquired = latch.await(timeout, TimeUnit.MILLISECONDS);
+                    if (!acquired) {
+                        throw new CacheException(
+                            "Couldn't get a lock in " + timeout + " for the key " + key + " at the cache " + delegate.getId());
+                    }
+                } else {
+                    latch.await();
+                }
+            } catch (InterruptedException e) {
+                throw new CacheException("Got interrupted while trying to acquire lock for key " + key, e);
+            }
+        }
+    }
+
+    private void releaseLock(Object key) {
+//        先从map里面删除key，然后释放一个锁
+        CountDownLatch latch = locks.remove(key);
+        if (latch == null) {
+            throw new IllegalStateException("Detected an attempt at releasing unacquired lock. This should never happen.");
+        }
+        latch.countDown();
+    }
+
+    public long getTimeout() {
+        return timeout;
+    }
+
+    public void setTimeout(long timeout) {
+        this.timeout = timeout;
+    }
 }
